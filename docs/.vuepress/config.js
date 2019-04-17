1d229dade5a005e1c3ff49f287f128a0c52caaa5
Tom Campbell <tomcampbell@gmail.com>
2019-04-17 11:18:49 -0700

    Establish docs base deployment directory

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = ctx => ({
+  // Directory from which this site is deployed
+  // (VuePress default but good practice for
+  // any software project because it leaves
+  // the root for code instead of documentation)
+  base: '/docs/',
+
+  title: '2019 WordPress Hosting Best Of',
+
+  // Update description metatag for search 
+  // engine display
+  description: 'How to find a cheap, fast WordPress host',
+
+  // Make it available offline
+  serviceWorker: true,
+})
+
